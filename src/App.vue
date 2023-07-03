5ffc073f7ee75a88ea9747fda57d892d3882db18
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-07-03 10:21:46 +0400

    [Lesson 7] added CLI version

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <header>
+      <h2>My Frineds</h2>
+    </header>
+    <ul>
+      <li>
+        <friend-contact></friend-contact>
+        <friend-contact></friend-contact>
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0102030405',
+          email: 'manuel@example.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '01452424405',
+          email: 'julie@example.com',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+ header {
+   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+ }
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone</strong> {{ friend.phone }} </li>
+      <li><strong>Email</strong> {{ friend.email }} </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '0102030405',
+        email: 'manuel@example.com',
+      },
+    };
+  },
+
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+</script>
+
+<style></style>
